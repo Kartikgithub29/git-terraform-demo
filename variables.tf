413dd5fad46722d90e30482e6f2266986cb85bdd
Kartik Saxena <kartiksaxena648@gmail.com>
2026-04-25 13:26:12 +0530

    Add variables for AMI and instance type

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+    description = "this is ami for instance"
+    type = string
+}
+
+variable "instance_type" {
+    description = "this is type for instance"
+    type = string
+  
+}
